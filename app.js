a145fcf3e7bec68c3e1abf2f1ab67442df074cce
myeong-ul <myeongul0412@gmail.com>
2023-01-03 01:17:50 +0900

    version 1.1

--- app.js
+++ app.js
@@ -5,20 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var novelRouter = require('./routes/novel');
 var aboutRouter = require('./routes/about');
 
 var main = express();
-//http to https redirect
-// main.use(function(req, res, next) {
-//     if (req.secure) {
-//         next();
-//     }
-//     else {
-//         res.redirect('https://' + req.headers.host + req.url);
-//     }
-// });
 
 // view engine setup
 main.set('views', path.join(__dirname, 'views'));
@@ -30,28 +20,18 @@ main.use(express.urlencoded({ extended: false }));
 main.use(cookieParser());
 main.use(express.static(path.join(__dirname, 'public')));
 
-main.get('/', async (req, res) => {
-  // res.sendFile(path.join(__dirname, 'index.html'));
-    res.render('index');
-});
 main.get('/img/:img', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'images', req.params.img));
+    res.sendFile(path.join(__dirname, 'public', 'images', req.params.img));
 });
 main.get('/css/:css', async (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'stylesheets', req.params.css));
 });
-main.get('/js/:js', async (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'javascripts', req.params.js));
-});
-main.get('/json/:json', async (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'json', req.params.json));
-});
 main.get('/short_novel_names', async (req, res) => {
     res.sendFile(path.join(__dirname, 'short_novel_names.html'));
 });
+
+main.use('/', indexRouter);
 main.use('/novel', novelRouter);
-main.use('/users', usersRouter);
-main.use('/index', indexRouter);
 main.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
@@ -60,13 +40,13 @@ main.use(function(req, res, next) {
 });
 
 // error handler
-main.use(function(err, req, res, next) {
+main.use(function(err, req, res) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    console.log("err");
   // render the error page
-  res.status(err.status || 500);
+    res.status(err.status || 500);
   //if status is 404, render 404 page
     res.render('error');
 });
--- routes/novel.js
+++ routes/novel.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
+var scheduler = require('node-schedule');
 
 var novel;
 fs.readFile(path.join(__dirname,'../public','json', 'novel.json'), 'utf8', function(err, data) {
@@ -9,9 +10,33 @@ fs.readFile(path.join(__dirname,'../public','json', 'novel.json'), 'utf8', funct
     novel = JSON.parse(data);
 });
 
+const schedule = scheduler.scheduleJob('0 0 0 */1 * *', function(){
+    fs.readFile(path.join(__dirname,'../public','json', 'novel.json'), 'utf8', function(err, data){
+        if (err) throw err;
+        novel = JSON.parse(data);
+        console.log("novel.json updated");
+    });
+});
+month = {
+    'Jan': '01',
+    'Feb': '02',
+    'Mar': '03',
+    'Apr': '04',
+    'May': '05',
+    'Jun': '06',
+    'Jul': '07',
+    'Aug': '08',
+    'Sep': '09',
+    'Oct': '10',
+    'Nov': '11',
+    'Dec': '12'
+}
 router.get('/', async (req, res) => {
-
-    res.render('novel', { title: '웹소설 단축어 목록', novels: novel});
+    var dates=schedule.nextInvocation().toString().split(' ');
+    var days=(Number(dates[2])-1).toString();
+    if (days.length===1) days='0'+days;
+    dates = "최근 수정 시각: "+dates[3] + '-' + month[dates[1]] + '-' + days + '-' + dates[4];
+    res.render('novel', {title: '웹소설 단축어 목록', novels: novel, time: dates});
 });
 
 module.exports = router;
