a2ca4a80c31c9683d570db7202100e71dc7a24ae
myeong-ul <100659123+myeong-ul@users.noreply.github.com>
2023-01-07 15:47:37 +0900

    DELETE log token
    Signed-off-by: myeong-ul <100659123+myeong-ul@users.noreply.github.com>

--- log.js
+++ log.js
@@ -2,49 +2,15 @@ const fs = require('fs')
 const path = require('path')
 const logger = require('morgan')
 
-process.env.TZ = 'Asia/Seoul'
-const red = "\x1B[31m",
-    green = "\x1b[32m",
-    yellow = "\x1b[33m",
-    cyan = "\x1b[36m",
-    white = "\x1B[37m",
-    endColor = "\033[0m",
-    blue = "\x1b[34m";
-logger.token('remote-addr', function (req, res) {
-    return blue + req.socket.remoteAddress + endColor;
-});
 logger.token('date', function (req, res) {
-    let d = new Date();
-    let date = new Date(d.getTime() - (d.getTimezoneOffset() * 60000)).toISOString().replace(/T/, ' ').split(' ');
-    if (Number(date[0].split('-')[2])%2 === 0) date = date[0] + green + " " + date[1] + endColor
-    else date = date[0] + cyan + " " + date[1] + endColor
-    return date
-});
-logger.token('status', function (req, res) {
-    let color
-    if (res.statusCode >= 500) color = red;
-    else if (res.statusCode >= 400) color = yellow;
-    else if (res.statusCode >= 300) color = cyan;
-    else if (res.statusCode >= 200) color = green;
-    else color = white;
-    return color + res.statusCode + endColor;
-});
-logger.token('method', function (req, res) {
-    let color
-    if (req.method === 'GET') color = green;
-    else if (req.method === 'POST') color = yellow;
-    else if (req.method === 'DELETE') color = red;
-    else color = white;
-    return color + req.method + endColor;
-});
-logger.token('url', function (req, res) {
-    return req.originalUrl;
+    return new Date().toLocaleString()
 });
 
 const accessLogStream = fs.createWriteStream(
-    path.join(__dirname, 'log', new Date().toISOString().split("T")[0] + ".ans"),
+    path.join(__dirname, 'log', new Date().toISOString().split("T")[0] + ".log"),
     {flags: 'a'}
 )
-const log = logger('[:remote-addr] [:date[web]] :method :url :status :response-time ms', {stream: accessLogStream})
+const format = ":date || :remote-addr :remote-user :method :url HTTP/:http-version :status - :response-time ms :referrer :user-agent"
+const log = logger(format, {stream: accessLogStream})
 
 module.exports = log
